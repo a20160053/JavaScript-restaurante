71e22c653d7990a5083b4d78a266a8897291bb4a
a20160053 <131028783+a20160053@users.noreply.github.com>
2023-09-11 12:59:17 -0500

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,11 @@
+alert("Bienvenido al restaurante. estsa es tu cuente");
+//Costo de Entrada
+var num1= parseInt(prompt("Ingrerse el costo de la entrada"));
+var num2= parseInt(prompt("Ingrese el costo de el segundo"));
+var num3= parseInt(prompt("Ingrese el costo del postre"));
+
+
+//Proseso de datos
+var suma = num1 + num2 + num3
+var toatl = suma
+var igv = (toatl*0.18)+toatl
